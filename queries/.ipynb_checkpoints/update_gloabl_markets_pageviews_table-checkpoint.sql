c285d4e32fac227917d812457b050f6a833976b9
Connie Chen <conniecc1@notebook1003.eqiad.wmnet>
2020-04-16 09:32:23 +0000

    Run Mar 2020 metrics

---
+++ queries/.ipynb_checkpoints/update_gloabl_markets_pageviews_table-checkpoint.sql
@@ -0,0 +1,22 @@
+INSERT INTO TABLE cchen.gs_pageviews_corrected
+PARTITION (year, month, day)
+
+SELECT date,
+countries.economic_region AS region,
+SUM(pageviews_country) AS pageviews,
+year, month, day
+FROM (
+  SELECT 
+  CONCAT(year,'-',LPAD(month,2,'0'),'-',LPAD(day,2,'0')) AS date,
+  country_code,
+  SUM(view_count) AS pageviews_country,
+  year, month, day
+  FROM wmf.pageview_hourly
+  WHERE (year = '{metrics_year}'AND month = '{metrics_cur_month}')
+  AND agent_type='user'
+  AND NOT (country_code IN ('PK', 'IR', 'AF') -- https://phabricator.wikimedia.org/T157404#3194046
+  AND user_agent_map['browser_family'] = 'IE') -- https://phabricator.wikimedia.org/T193578#4300284
+  GROUP BY year, month, day, country_code) AS bydatecountry
+JOIN canonical_data.countries AS countries
+ON bydatecountry.country_code = countries.iso_code
+GROUP BY date,year, month, day, countries.economic_region
